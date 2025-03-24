7ad324078d5e32b4281be96b7b5179575425ff10
ChristianCurtis06 <ctcurtis10@gmail.com>
2025-03-23 21:01:19 -0500

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth, Auth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFShrGo9rZTuITuO-d-FnRfL4PvKmcfBE",
+  apiKey: "...",
   authDomain: "e-commerce-assignment-9c693.firebaseapp.com",
   projectId: "e-commerce-assignment-9c693",
   storageBucket: "e-commerce-assignment-9c693.firebasestorage.app",
   messagingSenderId: "250588956187",
-  appId: "1:250588956187:web:487fcfc36b61e3907f9abe"
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
